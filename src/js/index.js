d036bf264cc04b00e49dc4afe11ce56d117ea905
Poulpynateur <nicolas.degheselle@viacesi.fr>
2020-02-16 22:53:27 +0100

    Color and responsivity set

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,57 @@
 require('../scss/styles.scss');
 
 function queryAddEvent(query, eventName, action) {
-    Array.from(document.querySelectorAll(query)).forEach(link => {
-        link.addEventListener(eventName, action);
+    Array.from(document.querySelectorAll(query)).forEach(element => {
+        element.addEventListener(eventName, (event) => {
+            action(element, event)
+        });
     });
 }
 
-queryAddEvent('.layout-navigator', 'click', (event) => {
+queryAddEvent('.layout-navigator', 'click', (source) => {
     Array.from(document.querySelectorAll('.layout, #welcome')).forEach((layout) => {
         layout.classList.remove('active');
     });
-    document.getElementById(event.target.dataset.target).classList.add('active');
+    document.getElementById(source.dataset.target).classList.add('active');
 });
 
-const maxScroll = Math.max( document.body.scrollHeight, document.body.offsetHeight, 
-    document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight );
-window.onscroll = () => {
-    console.log(window.scrollY, maxScroll);
+queryAddEvent('.button-collapse', 'click', (source) => {
+    document.getElementById(source.dataset.target).classList.toggle('active');
+});
+queryAddEvent('.button-collapse', 'mouseenter', (source) => {
+    document.getElementById(source.dataset.target).classList.add('active');
+});
+queryAddEvent('#menu-aside', 'mouseleave', (source) => {
+    document.getElementById(source.dataset.target).classList.remove('active');
+});
+
+/** Scroll **/
+const scrollOffset = 50;
+
+function sectionRelated(activeId) {
+    Array.from(document.querySelectorAll('.section-related')).forEach(element => {
+        element.classList.remove('active');
+        if (element.dataset.target === activeId) {
+            element.classList.add('active');
+        }
+    });
+}
+
+window.onscroll = function() {
+    // Active section
+    let scrollTarget;
+    Array.from(document.querySelectorAll('.section')).forEach(element => {
+        element.classList.remove('active');
+        if (element.getBoundingClientRect().top < scrollOffset)
+            scrollTarget = element;
+    });
+
+    scrollTarget.classList.add('active');
+    sectionRelated(scrollTarget.id);
+
+    // Nav menu
+    if (scrollTarget.id === 'welcome') 
+        this.document.getElementById('menu-aside').classList.remove('active');
+    else
+        this.document.getElementById('menu-aside').classList.add('active');
 };
